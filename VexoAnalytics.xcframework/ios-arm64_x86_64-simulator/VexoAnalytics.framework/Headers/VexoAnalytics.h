ce1a5d76fa93a9bf7697fcf0eeb9fb3d7bfe5502
Federico Nicoli <federiconicolig@gmail.com>
2024-10-03 18:01:56 -0300

    Version 0.2.0

--- VexoAnalytics.xcframework/ios-arm64_x86_64-simulator/VexoAnalytics.framework/Headers/VexoAnalytics.h
+++ VexoAnalytics.xcframework/ios-arm64_x86_64-simulator/VexoAnalytics.framework/Headers/VexoAnalytics.h
@@ -11,7 +11,7 @@
 
 + (void)configureWithApiKey:(NSString*_Nonnull)apikey deviceId:(NSString*_Nonnull)deviceId config:(NSDictionary*_Nonnull)config;
 + (void)startRecording:(NSString*_Nonnull)sessionId;
-+ (void)stopRecording:(NSString*_Nonnull)sessionId;
++ (void)stopRecording;
 + (NSString*_Nullable)blurImage:(NSString*_Nonnull)imageBase64
                          radius:(nonnull NSNumber*)radius
                           error:(NSError*_Nonnull*_Nonnull)errorPtr;
